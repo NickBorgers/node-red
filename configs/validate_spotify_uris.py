20bd85eed16808a6447034d8db07f4c943e945e2
Nick Borgers <nickborgers@gmail.com>
2022-10-02 09:19:41 -0400

    add my yaml configs

---
+++ configs/validate_spotify_uris.py
@@ -0,0 +1,17 @@
+import yaml
+import requests
+
+with open('music_config.yaml', 'r') as file:
+    music_config = yaml.safe_load(file)
+
+for music_type in music_config['music']:
+    music_type_object = music_config['music'][music_type]
+    for playback_option in music_type_object['playback_options']:
+        if playback_option['command'] == 'group.queue.urispotify':
+            components = playback_option['uri'].split(':')
+            resource = components[len(components)-1]
+            response = requests.get('https://open.spotify.com/playlist/' + resource)
+            if response.status_code != 200:
+              print("Could not find Spotify playlist identified by: " + resource)
+              print("Currently processing: " + music_type)
+              exit(1)
