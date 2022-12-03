5562c57d45a495174adc8a06cdec622501b075ad
Nick Borgers <nickborgers@gmail.com>
2022-12-03 16:24:29 -0600

    script works to screenshot each tab/flow, though unreliable

---
+++ .automated-rendering/screenshot-capture/tests/capture-screenshots-of-all-tabs.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+
+test('Capture screenshot of the entire scrollable webpage', async ({page}) => {
+    // Go to root page
+    await page.goto('/')
+    // Wait for load
+    await expect(page.locator('#red-ui-header-button-sidemenu')).toBeVisible({timeout: 2000})
+    // Close sidebars
+        // Close pallette
+            // Open top-right corner hamburger menu
+            await page.click('#red-ui-header-button-sidemenu')
+            // Wait for the menu to appear
+            await expect(page.locator('#red-ui-header-button-sidemenu-submenu')).toBeVisible({timeout: 500})
+            // Open "view" submenu
+            await page.hover('#menu-item-view-menu')
+            // Wait for submenu to appear
+            await expect(page.locator('#menu-item-view-menu-submenu')).toBeVisible({timeout: 500})
+            // Uncheck pallette
+            await page.click('#menu-item-palette')
+            // Wait for closure
+            await expect(page.locator('#red-ui-palette-container')).toBeHidden({timeout: 2000})
+        // Close sidebars
+            // Open top-right corner hamburger menu
+            await page.click('#red-ui-header-button-sidemenu')
+            // Wait for the menu to appear
+            await expect(page.locator('#red-ui-header-button-sidemenu-submenu')).toBeVisible({timeout: 500})
+            // Open "view" submenu
+            await page.hover('#menu-item-view-menu')
+            // Wait for submenu to appear
+            await expect(page.locator('#menu-item-view-menu-submenu')).toBeVisible({timeout: 500})
+            // Uncheck pallette
+            await page.click('#menu-item-sidebar')
+            // Wait for closure
+            await expect(page.locator('#red-ui-sidebar')).toBeHidden({timeout: 2000})
+
+    // Get tabs
+    // Collect tabs ids in this hacky way to return the list
+    var tab_ids = await page.evaluate('var children_ids = []; document.getElementById("red-ui-workspace-tabs").childNodes.forEach(function(this_node) {children_ids.push(this_node.id)}); children_ids ');
+    // Collect the flow names in this hacky way to return the list
+    var flow_names = await page.evaluate('var flow_names = []; document.getElementById("red-ui-workspace-tabs").childNodes.forEach(function(this_node) {flow_names.push(this_node.getAttribute("flowname"))}); flow_names ');
+    
+    console.log(tab_ids)
+    console.log(flow_names)
+    
+    // Create index variable
+    var index = 0;
+    // Do some ugly looping
+    while (true) {
+        // If there's a tab with this index
+        if (tab_ids[index]) {
+            // Set the tab id with the index
+            var this_tab_id = tab_ids[index]
+            // Set the flow name with the index
+            var this_flow_name = flow_names[index]
+            // Log that we're working this tab ID and flow name
+            console.log(`Working tab ${this_tab_id}, ${this_flow_name}`)
+            // Select the tab by clicking its tab element by id
+            await page.click(`#${this_tab_id}`)
+            // Hackily wait a half second for it to render
+            await page.waitForTimeout(500)
+            // Take that sweet screenshot
+            await page.screenshot({path: `screenshots/${this_flow_name}.png`, fullPage: true})
+        } else {
+            // If undefined, break
+            break
+        }
+        // increment iterator
+        index++;
+    }
+
+    // Yay we did it!
+  })
