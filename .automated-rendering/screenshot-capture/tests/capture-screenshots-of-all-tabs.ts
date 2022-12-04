d32504ec59f94bfb237ef8250ec727faaa5cbf39
Nick Borgers <nickborgers@gmail.com>
2022-12-04 12:58:07 -0500

    remove grid and make script re-runnable

--- .automated-rendering/screenshot-capture/tests/capture-screenshots-of-all-tabs.ts
+++ .automated-rendering/screenshot-capture/tests/capture-screenshots-of-all-tabs.ts
@@ -5,7 +5,11 @@ test('Capture screenshot of the entire scrollable webpage', async ({page}) => {
     await page.goto('/')
     // Wait for load
     await expect(page.locator('#red-ui-header-button-sidemenu')).toBeVisible({timeout: 2000})
-    // Close sidebars
+    // Bump up resolution
+    await page.setViewportSize({ width: 2400, height: 1200 });
+    // Close sidebars if open
+        const pallette = await page.$("#red-ui-palette-container")
+        if (await pallette.isVisible()) {
         // Close pallette
             // Open top-right corner hamburger menu
             await page.click('#red-ui-header-button-sidemenu')
@@ -19,6 +23,9 @@ test('Capture screenshot of the entire scrollable webpage', async ({page}) => {
             await page.click('#menu-item-palette')
             // Wait for closure
             await expect(page.locator('#red-ui-palette-container')).toBeHidden({timeout: 2000})
+        }
+        const sidebar = await page.$("#red-ui-sidebar")
+        if (await sidebar.isVisible()) {
         // Close sidebars
             // Open top-right corner hamburger menu
             await page.click('#red-ui-header-button-sidemenu')
@@ -32,7 +39,21 @@ test('Capture screenshot of the entire scrollable webpage', async ({page}) => {
             await page.click('#menu-item-sidebar')
             // Wait for closure
             await expect(page.locator('#red-ui-sidebar')).toBeHidden({timeout: 2000})
-
+        }
+    // Disable grid
+        // Open top-right corner hamburger menu
+        await page.click('#red-ui-header-button-sidemenu')
+        // Wait for the menu to appear
+        await expect(page.locator('#red-ui-header-button-sidemenu-submenu')).toBeVisible({timeout: 500})
+        // Go into settings menu
+        await page.click('#menu-item-user-settings')
+        // Wait for the menu to appear
+        await expect(page.locator('#user-settings-view-show-grid')).toBeVisible({timeout: 500})
+        // Uncheck show-grid option
+        await page.uncheck('#user-settings-view-show-grid')
+        // Save changes
+        await page.click('#node-dialog-ok')
+            
     // Get tabs
     // Collect tabs ids in this hacky way to return the list
     var tab_ids = await page.evaluate('var children_ids = []; document.getElementById("red-ui-workspace-tabs").childNodes.forEach(function(this_node) {children_ids.push(this_node.id)}); children_ids ');
@@ -56,7 +77,7 @@ test('Capture screenshot of the entire scrollable webpage', async ({page}) => {
             // Hackily wait a half second for it to render
             await page.waitForTimeout(500)
             // Take that sweet screenshot
-            await page.screenshot({path: `screenshots/${this_flow_name}.png`, fullPage: true})
+            await page.locator("#red-ui-workspace-chart").screenshot({path: `screenshots/${this_flow_name}.png`})
         } else {
             // If undefined, break
             break
